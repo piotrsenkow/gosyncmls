a03c759ad84f2a2acba8163391b38fb234824096
Piotr Senkow <piotrsenkow@Piotrs-MacBook-Pro.local>
2023-11-24 12:30:48 -0600

    adding change to go.mod referencing right github project and also adding new date type to handle inconsistent data coming in from mlsgrid

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"database/sql/driver"
 	"encoding/json"
 	"fmt"
 	"github.com/cenkalti/backoff/v4"
@@ -18,6 +19,7 @@ import (
 )
 
 type IntValue int
+type CustomTime time.Time
 
 type Property struct {
 	ListingId                    string    `json:"ListingId"`
@@ -39,7 +41,7 @@ type Property struct {
 	PostalCode                   string    `json:"PostalCode"`
 	CountyOrParish               string    `json:"CountyOrParish"`
 	Township                     string    `json:"Township"`
-	RoomsTotal                   int       `json:"RoomsTotal"`
+	RoomsTotal                   IntValue  `json:"RoomsTotal"`
 	BedroomsTotal                IntValue  `json:"BedroomsTotal"`
 	BathroomsFull                IntValue  `json:"BathroomsFull"`
 	BathroomsHalf                IntValue  `json:"BathroomsHalf"`
@@ -71,32 +73,32 @@ type Property struct {
 	LaundryFeatures       []string `json:"LaundryFeatures"`
 	Cooling               []string `json:"Cooling"`
 
-	MLSAreaMajor           string    `json:"MLSAreaMajor"`
-	MRD_ACTUALSTATUS       string    `json:"MRD_ACTUALSTATUS"`
-	MRD_ACTV_DATE          time.Time `json:"MRD_ACTV_DATE"`
-	AssociationFeeIncludes []string  `json:"AssociationFeeIncludes"`
-	MRD_ASQ                string    `json:"MRD_ASQ"`
-	MRD_ASSESSOR_SQFT      string    `json:"MRD_ASSESSOR_SQFT"`
-	MRD_BB                 string    `json:"MRD_BB"`
-	MRD_BLDG_ON_LAND       string    `json:"MRD_BLDG_ON_LAND"`
-	MRD_BMD                string    `json:"MRD_BMD"`
-	MRD_BRBELOW            string    `json:"MRD_BRBELOW"`
-	MRD_CAN_OWNER_RENT     string    `json:"MRD_CAN_OWNER_RENT"`
-	MRD_CURRENTLYLEASED    string    `json:"MRD_CURRENTLYLEASED"`
-	MRD_DEED_GARAGE_COST   string    `json:"MRD_DEED_GARAGE_COST"`
-	MRD_DIN                string    `json:"MRD_DIN"`
-	MRD_DISABILITY_ACCESS  string    `json:"MRD_DISABILITY_ACCESS"`
-	MRD_EXT                string    `json:"MRD_EXT"`
-	MRD_FIREPLACE_LOCATION string    `json:"MRD_FIREPLACE_LOCATION"`
-	MRD_FULL_BATHS_BLDG    string    `json:"MRD_FULL_BATHS_BLDG"`
-	MRD_GARAGE_ONSITE      string    `json:"MRD_GARAGE_ONSITE"`
-	MRD_GARAGE_OWNERSHIP   string    `json:"MRD_GARAGE_OWNERSHIP"`
-	MRD_GARAGE_TYPE        string    `json:"MRD_GARAGE_TYPE"`
-	MRD_SP_INCL_PARKING    string    `json:"MRD_SP_INCL_PARKING"`
-	MRD_HALF_BATHS_BLDG    string    `json:"MRD_HALF_BATHS_BLDG"`
-	MRD_IDX                string    `json:"MRD_IDX"`
-	MRD_LSZ                string    `json:"MRD_LSZ"`
-	MRD_MAF                string    `json:"MRD_MAF"`
+	MLSAreaMajor           string     `json:"MLSAreaMajor"`
+	MRD_ACTUALSTATUS       string     `json:"MRD_ACTUALSTATUS"`
+	MRD_ACTV_DATE          CustomTime `json:"MRD_ACTV_DATE"`
+	AssociationFeeIncludes []string   `json:"AssociationFeeIncludes"`
+	MRD_ASQ                string     `json:"MRD_ASQ"`
+	MRD_ASSESSOR_SQFT      string     `json:"MRD_ASSESSOR_SQFT"`
+	MRD_BB                 string     `json:"MRD_BB"`
+	MRD_BLDG_ON_LAND       string     `json:"MRD_BLDG_ON_LAND"`
+	MRD_BMD                string     `json:"MRD_BMD"`
+	MRD_BRBELOW            string     `json:"MRD_BRBELOW"`
+	MRD_CAN_OWNER_RENT     string     `json:"MRD_CAN_OWNER_RENT"`
+	MRD_CURRENTLYLEASED    string     `json:"MRD_CURRENTLYLEASED"`
+	MRD_DEED_GARAGE_COST   string     `json:"MRD_DEED_GARAGE_COST"`
+	MRD_DIN                string     `json:"MRD_DIN"`
+	MRD_DISABILITY_ACCESS  string     `json:"MRD_DISABILITY_ACCESS"`
+	MRD_EXT                string     `json:"MRD_EXT"`
+	MRD_FIREPLACE_LOCATION string     `json:"MRD_FIREPLACE_LOCATION"`
+	MRD_FULL_BATHS_BLDG    string     `json:"MRD_FULL_BATHS_BLDG"`
+	MRD_GARAGE_ONSITE      string     `json:"MRD_GARAGE_ONSITE"`
+	MRD_GARAGE_OWNERSHIP   string     `json:"MRD_GARAGE_OWNERSHIP"`
+	MRD_GARAGE_TYPE        string     `json:"MRD_GARAGE_TYPE"`
+	MRD_SP_INCL_PARKING    string     `json:"MRD_SP_INCL_PARKING"`
+	MRD_HALF_BATHS_BLDG    string     `json:"MRD_HALF_BATHS_BLDG"`
+	MRD_IDX                string     `json:"MRD_IDX"`
+	MRD_LSZ                string     `json:"MRD_LSZ"`
+	MRD_MAF                string     `json:"MRD_MAF"`
 
 	GrossIncome          IntValue `json:"GrossIncome"`
 	AdditionalParcelsYN  bool     `json:"AdditionalParcelsYN"`
@@ -116,16 +118,16 @@ type Property struct {
 	MRD_MIN_RP           string   `json:"MRD_MIN_RP"`
 	MRD_MAX_RP           string   `json:"MRD_MAX_RP"`
 
-	CumulativeDaysOnMarket int    `json:"CumulativeDaysOnMarket"`
-	LeaseTerm              string `json:"LeaseTerm"`
-	MRD_NEW_CONSTR_YN      string `json:"MRD_NEW_CONSTR_YN"`
-	MRD_ORP                string `json:"MRD_ORP"`
-	MRD_AON                string `json:"MRD_AON"`
-	MRD_B78                string `json:"MRD_B78"`
-	MRD_BAS                string `json:"MRD_BAS"`
-	MRD_BD3                string `json:"MRD_BD3"`
-	CloseDate              string `json:"CloseDate"`
-	FrontageLength         string `json:"FrontageLength"`
+	CumulativeDaysOnMarket IntValue `json:"CumulativeDaysOnMarket"`
+	LeaseTerm              string   `json:"LeaseTerm"`
+	MRD_NEW_CONSTR_YN      string   `json:"MRD_NEW_CONSTR_YN"`
+	MRD_ORP                string   `json:"MRD_ORP"`
+	MRD_AON                string   `json:"MRD_AON"`
+	MRD_B78                string   `json:"MRD_B78"`
+	MRD_BAS                string   `json:"MRD_BAS"`
+	MRD_BD3                string   `json:"MRD_BD3"`
+	CloseDate              string   `json:"CloseDate"`
+	FrontageLength         string   `json:"FrontageLength"`
 
 	MRD_PARKING_ONSITE       string `json:"MRD_PARKING_ONSITE"`
 	MRD_PKN                  string `json:"MRD_PKN"`
@@ -152,38 +154,38 @@ type Property struct {
 	MRD_UD                   string `json:"MRD_UD"`
 	MRD_UFL                  string `json:"MRD_UFL"`
 
-	NetOperatingIncome     IntValue  `json:"NetOperatingIncome"`
-	NewConstructionYN      bool      `json:"NewConstructionYN"`
-	OffMarketDate          string    `json:"OffMarketDate"`
-	OperatingExpense       IntValue  `json:"OperatingExpense"`
-	OriginalEntryTimestamp time.Time `json:"OriginalEntryTimestamp"`
-	OtherEquipment         []string  `json:"OtherEquipment"`
-	OtherStructures        []string  `json:"OtherStructures"`
-	ParkingTotal           IntValue  `json:"ParkingTotal"`
-	PostalCodePlus4        string    `json:"PostalCodePlus4"`
-	PreviousListPrice      int       `json:"PreviousListPrice"`
-	PurchaseContractDate   string    `json:"PurchaseContractDate"`
-	RentIncludes           []string  `json:"RentIncludes"`
-	StandardStatus         string    `json:"StandardStatus"`
-	StateOrProvince        string    `json:"StateOrProvince"`
-	StatusChangeTimestamp  time.Time `json:"StatusChangeTimestamp"`
-	StreetDirPrefix        string    `json:"StreetDirPrefix"`
-	StreetSuffix           string    `json:"StreetSuffix"`
-	TotalActualRent        IntValue  `json:"TotalActualRent"`
-	TrashExpense           IntValue  `json:"TrashExpense"`
-	WaterSewerExpense      IntValue  `json:"WaterSewerExpense"`
-	Zoning                 string    `json:"Zoning"`
-	ListAgentEmail         string    `json:"ListAgentEmail"`
-	ListAgentFirstName     string    `json:"ListAgentFirstName"`
-	ListAgentLastName      string    `json:"ListAgentLastName"`
-	ListAgentFullName      string    `json:"ListAgentFullName"`
-	ListAgentMlsId         string    `json:"ListAgentMlsId"`
-	ListAgentMobilePhone   string    `json:"ListAgentMobilePhone"`
-	ListAgentKey           string    `json:"ListAgentKey"`
-	ListOfficeMlsId        string    `json:"ListOfficeMlsId"`
-	ListOfficeName         string    `json:"ListOfficeName"`
-	ListOfficePhone        string    `json:"ListOfficePhone"`
-	ListingContractDate    string    `json:"ListingContractDate"`
+	NetOperatingIncome     IntValue   `json:"NetOperatingIncome"`
+	NewConstructionYN      bool       `json:"NewConstructionYN"`
+	OffMarketDate          string     `json:"OffMarketDate"`
+	OperatingExpense       IntValue   `json:"OperatingExpense"`
+	OriginalEntryTimestamp CustomTime `json:"OriginalEntryTimestamp"`
+	OtherEquipment         []string   `json:"OtherEquipment"`
+	OtherStructures        []string   `json:"OtherStructures"`
+	ParkingTotal           IntValue   `json:"ParkingTotal"`
+	PostalCodePlus4        string     `json:"PostalCodePlus4"`
+	PreviousListPrice      IntValue   `json:"PreviousListPrice"`
+	PurchaseContractDate   string     `json:"PurchaseContractDate"`
+	RentIncludes           []string   `json:"RentIncludes"`
+	StandardStatus         string     `json:"StandardStatus"`
+	StateOrProvince        string     `json:"StateOrProvince"`
+	StatusChangeTimestamp  CustomTime `json:"StatusChangeTimestamp"`
+	StreetDirPrefix        string     `json:"StreetDirPrefix"`
+	StreetSuffix           string     `json:"StreetSuffix"`
+	TotalActualRent        IntValue   `json:"TotalActualRent"`
+	TrashExpense           IntValue   `json:"TrashExpense"`
+	WaterSewerExpense      IntValue   `json:"WaterSewerExpense"`
+	Zoning                 string     `json:"Zoning"`
+	ListAgentEmail         string     `json:"ListAgentEmail"`
+	ListAgentFirstName     string     `json:"ListAgentFirstName"`
+	ListAgentLastName      string     `json:"ListAgentLastName"`
+	ListAgentFullName      string     `json:"ListAgentFullName"`
+	ListAgentMlsId         string     `json:"ListAgentMlsId"`
+	ListAgentMobilePhone   string     `json:"ListAgentMobilePhone"`
+	ListAgentKey           string     `json:"ListAgentKey"`
+	ListOfficeMlsId        string     `json:"ListOfficeMlsId"`
+	ListOfficeName         string     `json:"ListOfficeName"`
+	ListOfficePhone        string     `json:"ListOfficePhone"`
+	ListingContractDate    string     `json:"ListingContractDate"`
 
 	UnitTypes []UnitType `json:"UnitTypes"`
 	Rooms     []Room     `json:"Rooms"`
@@ -199,13 +201,13 @@ type Room struct {
 }
 
 type UnitType struct {
-	UnitTypeKey         string `json:"UnitTypeKey"`
-	FloorNumber         string `json:"MRD_FloorNumber"`
-	UnitNumber          string `json:"UnitTypeType"`
-	UnitBedroomsTotal   int    `json:"UnitTypeBedsTotal"`
-	UnitBathroomsTotal  int    `json:"UnitTypeBathsTotal"`
-	UnitTotalRent       int    `json:"UnitTypeActualRent"`
-	UnitSecurityDeposit string `json:"MRD_SecurityDeposit"`
+	UnitTypeKey         string   `json:"UnitTypeKey"`
+	FloorNumber         string   `json:"MRD_FloorNumber"`
+	UnitNumber          string   `json:"UnitTypeType"`
+	UnitBedroomsTotal   IntValue `json:"UnitTypeBedsTotal"`
+	UnitBathroomsTotal  IntValue `json:"UnitTypeBathsTotal"`
+	UnitTotalRent       IntValue `json:"UnitTypeActualRent"`
+	UnitSecurityDeposit string   `json:"MRD_SecurityDeposit"`
 }
 
 type Media struct {
@@ -263,6 +265,36 @@ func (iv *IntValue) UnmarshalJSON(b []byte) error {
 		return fmt.Errorf("invalid format for integer value")
 	}
 	return nil
+}
+
+func (ct *CustomTime) UnmarshalJSON(b []byte) error {
+	var strTime string
+	if err := json.Unmarshal(b, &strTime); err != nil {
+		return err
+	}
+
+	parsedTime, err := time.Parse(time.RFC3339, strTime)
+	if err != nil {
+		// Try parsing without timezone information
+		parsedTime, err = time.Parse("2006-01-02T15:04:05", strTime)
+		if err != nil {
+			return err
+		}
+	}
+
+	*ct = CustomTime(parsedTime)
+	return nil
+}
+
+// Implement driver.Valuer interface
+func (ct CustomTime) Value() (driver.Value, error) {
+	// Convert CustomTime to time.Time, then to driver.Value (which is just interface{})
+	t := time.Time(ct)
+	if t.IsZero() {
+		// Return NULL if zero time
+		return nil, nil
+	}
+	return t, nil
 }
 
 func (cr *countingReader) Read(p []byte) (int, error) {
